f1419d82a741f8eae298f64dbe7f9fa106348ad4
traderjeffb <traderjeffb@gmail.com>
2021-06-28 09:17:22 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import FlashcardList from './FlashcardList';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const [flashcards, setFlashcards]= useState(SAMPLE_FLASHCARDS)
+  return ( 
+    <FlashcardList flashcards={flashcards} />
   );
 }
+
+const SAMPLE_FLASHCARDS = [
+    {
+      id: 1,
+      question: 'what is 2+2',
+      answer: '4',
+      options: [
+      '2',
+      '3',
+      '4',
+      '5'
+    ]
+  },
+  {
+    id: 2,
+    question: 'what is 3-1',
+    answer: '4',
+    options: [
+    '2',
+    '3',
+    '4',
+    '5'
+    ]
+  }
+]
 
 export default App;
